5c9226ac1e6b44e9760ba7b9a95ccad5f31becd1
William Santana de Amorim <williamsantanadeamorim@MacBook-Air-de-William.local>
2024-09-22 21:06:19 -0400

    first commit

---
+++ Models/UserModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web_.NET_AT_Exerc05.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O e-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "Formato de e-mail inválido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A data de nascimento é obrigatória.")]
+        [DataType(DataType.Date)]
+        [BirthDateValidation(ErrorMessage = "A data de nascimento não pode ser no futuro.")]
+        public DateTime BirthDate { get; set; }
+    }
+
+    public class BirthDateValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var birthDate = (DateTime)value;
+            if (birthDate > DateTime.Now)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ Pages/UserForm.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Web_.NET_AT_Exerc05.Models;
+
+namespace Web_.NET_AT_Exerc05.Pages
+{
+    public class UserFormModel : PageModel
+    {
+        [BindProperty]
+        public UserModel User { get; set; }
+
+        public string Message { get; private set; }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            Message = "Dados enviados com sucesso!";
+            return Page();
+        }
+    }
+}
